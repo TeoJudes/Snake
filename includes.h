96e08cf349c4be6f243fa502abff6d38269f5d6f
Téo Judes <judesteo@laposte.net>
2015-05-29 06:23:55 +0200

    Update includes.h

--- includes.h
+++ includes.h
@@ -10,43 +10,43 @@
 
 #define WIDTH BLOCK * BLOCK_WIDTH
 #define HEIGHT BLOCK * BLOCK_HEIGHT
-#define BLOCK 20
-#define BLOCK_WIDTH 70      //nombres de blocks en vertical du quadrillage
-#define BLOCK_HEIGHT 45    //idem en horizontal
+#define BLOCK 20            //taille d'un bloc en pixel
+#define BLOCK_WIDTH 40     //nombres de blocks en vertical du quadrillage
+#define BLOCK_HEIGHT 30    //idem en horizontal
 
 //suivent les emplacement des bouttons du menu
-#define PlayLx 314
-#define PlayRx 517
+#define PlayLx 430
+#define PlayRx 640
 #define PlayTy 17
 #define PlayBy 76
 
-#define InstruLx 250
-#define InstruRx 590
+#define InstruLx 350
+#define InstruRx 780
 #define InstruTy 77
 #define InstruBy 141
 
-#define ScoreLx 314
-#define ScoreRx 517
+#define ScoreLx 430
+#define ScoreRx 630
 #define ScoreTy 142
 #define ScoreBy 209
 
-#define ExitLx 314
-#define ExitRx 517
+#define ExitLx 420
+#define ExitRx 690
 #define ExitTy 210
 #define ExitBy 282
 
-#define ScoreReturnMenuLx 245
-#define ScoreReturnMenuRx 577
+#define ScoreReturnMenuLx 330
+#define ScoreReturnMenuRx 760
 #define ScoreReturnMenuTy 5
 #define ScoreReturnMenuBy 66
 
-#define InstruExitLx 312
-#define InstruExitRx 580
+#define InstruExitLx 410
+#define InstruExitRx 770
 #define InstruExitTy 535
 #define InstruExitBy 580
 
 #define InstruReturnMenuLx 20
-#define InstruReturnMenuRx 290
+#define InstruReturnMenuRx 390
 #define InstruReturnMenuTy 535
 #define InstruReturnMenuBy 580
 
